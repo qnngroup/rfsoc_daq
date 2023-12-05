cf4534ec306d6e9d0afa656b47fe261926420fcf
Reed Foster <reed.a.foster@gmail.com>
2023-12-05 17:31:28 -0500

    scripts seem to be working okay now

--- script/make_project.tcl
+++ script/make_project.tcl
@@ -2,25 +2,30 @@
 set dir [file dirname [file normalize [info script]]]
 
 # get all synth RTL and simulation files
-set sim_files [glob $dir/../src/verif/*]
-set rtl_files [glob $dir/../src/rtl/*]
-set constriant_files [glob $dir/../src/constraints/*]
-
-puts $dir
-puts $sim_files
-puts $rtl_files
+set sim_files [glob -nocomplain "$dir/../src/verif/*"]
+set rtl_files [glob -nocomplain "$dir/../src/rtl/*"]
+set constraint_files [glob -nocomplain "$dir/../src/constraints/*"]
 
 # clean up project directory
-file delete -force -- $dir/../build/proj/*
+set proj_files [glob -nocomplain "$dir/../build/proj/*"]
+if {[llength $proj_files] != 0} {
+  file delete -force -- {*}[glob "$dir/../build/proj/*"]
+}
 
 # create a project
 create_project proj $dir/../build/proj -part xczu28dr-ffvg1517-2-e
 set_property board_part xilinx.com:zcu111:part0:1.4 [current_project]
 
 # add files
-add_files -fileset sim_1 -norecurse $sim_files
-add_files -fileset sources_1 -norecurse $rtl_files
-add_files -fileset constrs_1 -norecurse $constraint_files
+if {[llength $sim_files] != 0} {
+  add_files -fileset sim_1 -norecurse $sim_files
+}
+if {[llength $rtl_files] != 0} {
+  add_files -fileset sources_1 -norecurse $rtl_files
+}
+if {[llength $constraint_files] != 0} {
+  add_files -fileset constrs_1 -norecurse $constraint_files
+}
 
 # include all source files in simulation
 set_property SOURCE_SET sources_1 [get_filesets sim_1]
