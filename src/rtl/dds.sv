d8af20092282e2983435cc19b106c4a902df6c3e
reed-foster <reed.a.foster@gmail.com>
2023-12-17 20:19:30 -0500

    fix small bugs in DDS [(probably will never be relevant, but now should be working properly with non-continuous ready on output interface). Also implemented an actual test

--- src/rtl/dds.sv
+++ src/rtl/dds.sv
@@ -7,8 +7,7 @@ module dds #(
   input wire clk, reset,
   output logic pinc_change,
   Axis_If.Master_Simple cos_out,
-  Axis_If.Slave_Simple phase_inc_in,
-  Axis_If.Slave_Simple cos_scale_in
+  Axis_If.Slave_Simple phase_inc_in
 );
 
 
@@ -16,7 +15,6 @@ localparam int LUT_ADDR_BITS = PHASE_BITS - QUANT_BITS;
 localparam int LUT_DEPTH = 2**LUT_ADDR_BITS;
 
 assign phase_inc_in.ready = 1'b1;
-assign cos_scale_in.ready = 1'b1;
 
 // generate LUT
 localparam real PI = 3.14159265;
@@ -32,15 +30,21 @@ logic [PARALLEL_SAMPLES-1:0][PHASE_BITS-1:0] phase_inc;
 logic [PHASE_BITS-1:0] cycle_phase;
 logic [PARALLEL_SAMPLES-1:0][PHASE_BITS-1:0] sample_phase;
 assign phase_inc_in.ready = 1'b1;
-always @(posedge clk) begin
+
+// delay data_valid to match phase increment + LUT latency
+logic [4:0] data_valid;
+assign cos_out.valid = data_valid[4];
+
+always_ff @(posedge clk) begin
   if (reset) begin
     phase_inc <= '0;
     cycle_phase <= '0;
     sample_phase <= '0;
     pinc_change <= 1'b0;
+    data_valid <= '0;
   end else begin
     // load new phase_inc
-    if (phase_inc_in.ready && phase_inc_in.valid) begin
+    if (phase_inc_in.ok) begin
       pinc_change <= 1'b1;
       for (int i = 0; i < PARALLEL_SAMPLES; i = i + 1) begin
         phase_inc[i] <= phase_inc_in.data * (i + 1);
@@ -48,12 +52,13 @@ always @(posedge clk) begin
     end else begin
       pinc_change <= 1'b0;
     end
-    if (cos_out.ready) begin
+    if (cos_out.ready || ~cos_out.valid) begin
       cycle_phase <= cycle_phase + phase_inc[PARALLEL_SAMPLES-1];
       sample_phase[0] <= cycle_phase;
       for (int i = 1; i < PARALLEL_SAMPLES; i = i + 1) begin
         sample_phase[i] <= cycle_phase + phase_inc[i-1];
       end
+      data_valid <= {data_valid[3:0], 1'b1};
     end
   end
 end
@@ -63,52 +68,28 @@ logic [PARALLEL_SAMPLES-1:0][15:0] lfsr;
 lfsr16_parallel #(.PARALLEL_SAMPLES(PARALLEL_SAMPLES)) lfsr_i (
   .clk,
   .reset,
-  .enable(cos_out.ready),
+  .enable(cos_out.ready || ~cos_out.valid),
   .data_out(lfsr)
 );
 
 logic [PARALLEL_SAMPLES-1:0][PHASE_BITS-1:0] phase_dithered;
 logic [PARALLEL_SAMPLES-1:0][LUT_ADDR_BITS-1:0] phase_quant;
-// delay data_valid to match phase increment + LUT latency
-logic [3:0] data_valid;
-assign cos_out.valid = data_valid[3];
-// this isn't working right --- need to fix sign extension, and a bunch of samples are getting zeroed out
-logic signed [OUTPUT_WIDTH-1:0] data_out_full_scale [PARALLEL_SAMPLES];
-logic [$clog2(OUTPUT_WIDTH)-1:0] cos_scale;
-genvar i;
-generate
-  for (i = 0; i < PARALLEL_SAMPLES; i = i + 1) begin
-    if (QUANT_BITS > 16) begin
-      assign phase_dithered[i] = {lfsr[i], {(QUANT_BITS - 16){1'b0}}} + sample_phase[i];
-    end else begin
-      assign phase_dithered[i] = lfsr[i][QUANT_BITS-1:0] + sample_phase[i];
-    end
-    always @(posedge clk) begin
-      cos_out.data[OUTPUT_WIDTH*i+:OUTPUT_WIDTH] <= data_out_full_scale[i] >>> cos_scale;
-      if (reset) begin
-        data_out_full_scale[i] <= '0;
-        phase_quant[i] <= '0;
-      end else begin
-        if (cos_out.ready) begin
-          phase_quant[i] <= phase_dithered[i][PHASE_BITS-1:QUANT_BITS];
-          data_out_full_scale[i] <= lut[phase_quant[i]];
-        end
-      end
-    end
-  end
-endgenerate
-
-always @(posedge clk) begin
+always_ff @(posedge clk) begin
   if (reset) begin
-    cos_scale <= '0;
-    data_valid <= '0;
+    cos_out.data <= '0;
+    phase_quant <= '0;
+    phase_dithered <= '0;
   end else begin
-    if (cos_scale_in.valid) begin
-      cos_scale <= cos_scale_in.data;
-    end
-    data_valid[0] <= 1'b1;
-    for (int j = 1; j < 4; j++) begin
-      data_valid[j] <= data_valid[j-1];
+    if (cos_out.ready || ~cos_out.valid) begin
+      for (int i = 0; i < PARALLEL_SAMPLES; i++) begin
+        if (QUANT_BITS > 16) begin
+          phase_dithered[i] <= {lfsr[i], {(QUANT_BITS - 16){1'b0}}} + sample_phase[i];
+        end else begin
+          phase_dithered[i] <= lfsr[i][QUANT_BITS-1:0] + sample_phase[i];
+        end
+        phase_quant[i] <= phase_dithered[i][PHASE_BITS-1:QUANT_BITS];
+        cos_out.data[OUTPUT_WIDTH*i+:OUTPUT_WIDTH] <= lut[phase_quant[i]];
+      end
     end
   end
 end
--- src/verif/dds_test.sv
+++ src/verif/dds_test.sv
@@ -3,15 +3,6 @@ import sim_util_pkg::*;
 `timescale 1ns / 1ps
 module dds_test ();
 
-// TODO actuallly check output is correct
-sim_util_pkg::generic #(int) util; // abs, max functions on ints
-sim_util_pkg::debug #(.VERBOSITY(DEFAULT)) dbg = new; // printing, error tracking
-
-logic reset;
-logic clk = 0;
-localparam CLK_RATE_HZ = 100_000_000;
-always #(0.5s/CLK_RATE_HZ) clk = ~clk;
-
 localparam PHASE_BITS = 24;
 localparam OUTPUT_WIDTH = 18;
 localparam QUANT_BITS = 8;
@@ -19,79 +10,140 @@ localparam PARALLEL_SAMPLES = 4;
 localparam LUT_ADDR_BITS = PHASE_BITS - QUANT_BITS;
 localparam LUT_DEPTH = 2**LUT_ADDR_BITS;
 
+typedef logic signed [OUTPUT_WIDTH-1:0] sample_t;
+typedef logic [PHASE_BITS-1:0] phase_t;
+
+sim_util_pkg::generic #(sample_t) util; // abs, max functions on sample_t
+sim_util_pkg::debug #(.VERBOSITY(DEFAULT)) dbg = new; // printing, error tracking
+
+logic reset;
+logic clk = 0;
+localparam CLK_RATE_HZ = 100_000_000;
+always #(0.5s/CLK_RATE_HZ) clk = ~clk;
+
 Axis_If #(.DWIDTH(PHASE_BITS)) phase_inc_in();
 Axis_If #(.DWIDTH(OUTPUT_WIDTH*PARALLEL_SAMPLES)) cos_out();
-Axis_If #(.DWIDTH($clog2(OUTPUT_WIDTH))) cos_scale_in();
+
+// easier debugging in waveform view
+sample_t cos_out_split [PARALLEL_SAMPLES];
+always_comb begin
+  for (int i = 0; i < PARALLEL_SAMPLES; i++) begin
+    cos_out_split[i] <= cos_out.data[OUTPUT_WIDTH*i+:OUTPUT_WIDTH];
+  end
+end
 
 dds #(.PHASE_BITS(PHASE_BITS), .OUTPUT_WIDTH(OUTPUT_WIDTH), .QUANT_BITS(QUANT_BITS)) dut_i (
   .clk,
   .reset,
   .cos_out,
-  .phase_inc_in,
-  .cos_scale_in
+  .phase_inc_in
 );
 
 localparam int N_FREQS = 4;
-localparam int N_SAMP_PER_FREQ = 2**14;
 int freqs [N_FREQS] = {12_130_000, 517_036_000, 1_729_725_000, 2_759_000};
 
-logic [PHASE_BITS-1:0] phase_inc, phase;
-logic [PARALLEL_SAMPLES:0][PHASE_BITS-1:0] sample_phases;
-
-logic [PARALLEL_SAMPLES-1:0][OUTPUT_WIDTH-1:0] cos_dout;
+phase_t phase_inc, phase_inc_prev, phase;
+sample_t received [$];
 
 localparam real PI = 3.14159265;
-logic signed [OUTPUT_WIDTH-1:0] test_lut [LUT_DEPTH];
 
-function logic[PHASE_BITS-1:0] get_phase_inc_from_freq(input int freq);
+function phase_t get_phase_inc_from_freq(input int freq);
   return unsigned'(int'($floor((real'(freq)/6_400_000_000.0) * (2**(PHASE_BITS)))));
 endfunction
 
 always @(posedge clk) begin
   if (reset) begin
-    phase <= '0;
     phase_inc <= '0;
   end else begin
-    if (phase_inc_in.valid && phase_inc_in.ready) begin
+    if (phase_inc_in.ok) begin
       phase_inc <= phase_inc_in.data;
     end
-    for (int i = 0; i < PARALLEL_SAMPLES + 1; i++) begin
-      sample_phases[i] <= phase_inc * i;
-    end
-    if (cos_out.ready && cos_out.valid) begin
-      phase <= phase + sample_phases[PARALLEL_SAMPLES];
-    end
-    for (int i = 0; i < PARALLEL_SAMPLES; i++) begin
-      cos_dout[i] <= test_lut[(phase + sample_phases[i]) >> QUANT_BITS];
+    if (cos_out.ok) begin
+      for (int i = 0; i < PARALLEL_SAMPLES; i++) begin
+        received.push_front(sample_t'(cos_out.data[i*OUTPUT_WIDTH+:OUTPUT_WIDTH]));
+      end
     end
   end
 end
+
+task automatic check_output(inout phase_t phase, input phase_t phase_inc, input phase_t phase_inc_prev);
+  sample_t expected;
+  int count;
+  count = 0;
+  dbg.display($sformatf(
+    "checking output with initial phase %x, phase_inc %x, phase_inc_prev %x",
+    phase,
+    phase_inc,
+    phase_inc_prev),
+    VERBOSE
+  );
+  while (received.size() > 0) begin
+    // first four 12 samples are with previous phase inc
+    expected = sample_t'($floor((2**(OUTPUT_WIDTH-1) - 0.5)*$cos(2*PI/real'(2**PHASE_BITS)*real'(phase))-0.5));
+    if (util.abs(received[$] - expected) > 4'hf) begin
+      dbg.error($sformatf(
+        "mismatched sample value for phase = %x: expected %x got %x",
+        phase,
+        expected,
+        received[$])
+      );
+    end
+    dbg.display($sformatf(
+      "got phase/sample pair: %x, %x",
+      phase,
+      received[$]),
+      DEBUG
+    );
+    // 5 cycles of latency
+    if (count < 4*PARALLEL_SAMPLES) begin
+      phase = phase + phase_inc_prev;
+    end else begin
+      phase = phase + phase_inc;
+    end
+    received.pop_back();
+    count = count + 1;
+  end
+endtask
 
 initial begin
   dbg.display("################################", DEFAULT);
   dbg.display("# testing dds signal generator #", DEFAULT);
   dbg.display("################################", DEFAULT);
-  for (int i = 0; i < LUT_DEPTH; i = i + 1) begin
-    test_lut[i] <= signed'(int'($floor($cos(2*PI/(LUT_DEPTH)*i)*(2**(OUTPUT_WIDTH-1) - 0.5) - 0.5)));
-  end
   reset <= 1'b1;
+  cos_out.ready <= 1'b0;
+  phase <= '0;
+  phase_inc_prev <= '0;
   repeat (50) @(posedge clk);
   reset <= 1'b0;
   repeat (20) @(posedge clk);
-  cos_out.ready <= 1'b1;
-  cos_scale_in.data <= 4'b1;
-  cos_scale_in.valid <= 1'b1;
-  //repeat (100) @(posedge clk);
   for (int i = 0; i < N_FREQS; i++) begin
     phase_inc_in.data <= get_phase_inc_from_freq(freqs[i]);
     phase_inc_in.valid <= 1;
     repeat (1) @(posedge clk);
     phase_inc_in.valid <= 0;
     repeat (100) @(posedge clk);
+    // wait a few cycles before asserting cos_out.ready to ensure that any
+    // phase_inc changes have propagated
+    // this makes it easier to test for correct behavior
+    // TODO actually implement the correct latency tracking so that the
+    // verification works properly regardless of whether the phase increment
+    // is changed while the output is active
+    cos_out.ready <= 1'b1;
+    repeat (100) begin
+      @(posedge clk);
+      cos_out.ready <= $urandom_range(0,1) & 1'b1;
+    end
+    cos_out.ready <= 1'b1;
+    repeat (100) @(posedge clk);
     cos_out.ready <= 1'b0;
     repeat (100) @(posedge clk);
-    cos_out.ready <= 1'b1;
-    repeat (3995) @(posedge clk);
+    dbg.display($sformatf(
+      "checking behavior for freq = %0d Hz",
+      freqs[i]),
+      VERBOSE
+    );
+    check_output(phase, phase_inc, phase_inc_prev);
+    phase_inc_prev <= phase_inc;
   end
   dbg.finish();
 end
