06c1c3a7157aeb696c479c04fc2da4aa89f191c1
reed-foster <reed.a.foster@gmail.com>
2023-12-20 10:45:16 -0500

    regression test works now

--- script/regression.sh
+++ script/regression.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
-modules=$(grep -Pho 'module ([a-zA-Z0-9_]*)_test' ../src/verif/* | sed 's/^module //g')
+# https://askubuntu.com/questions/893911/when-writing-a-bash-script-how-do-i-get-the-absolute-path-of-the-location-of-th
+script_dir=$(dirname "$(realpath $0)")
+
+modules=$(grep -Pho 'module ([a-zA-Z0-9_]*)_test' $script_dir/../src/verif/* | sed 's/^module //g')
 
 echo "###########################"
 echo "# Running regression test #"
 echo "###########################"
 echo "modules to test: $modules"
-
-# https://askubuntu.com/questions/893911/when-writing-a-bash-script-how-do-i-get-the-absolute-path-of-the-location-of-th
-script_dir=$(dirname "$(realpath $0)")
 
 # https://unix.stackexchange.com/questions/29509/transform-an-array-into-arguments-of-a-command
 vivado -mode batch -source $script_dir/run_unit_test.tcl -tclargs $modules
