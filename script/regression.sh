229de3d2111534e0aede04359461c69075d82602
reed-foster <reed.a.foster@gmail.com>
2023-12-30 15:34:49 -0500

    reorganized more, now hierarchy is a fair bit more clear

--- script/regression.sh
+++ script/regression.sh
@@ -3,12 +3,12 @@
 # https://askubuntu.com/questions/893911/when-writing-a-bash-script-how-do-i-get-the-absolute-path-of-the-location-of-th
 script_dir=$(dirname "$(realpath $0)")
 
-modules=$(grep -Pho 'module ([a-zA-Z0-9_]*)_test' $script_dir/../src/verif/* | sed 's/^module //g')
+modules=$(grep -RPho 'module ([a-zA-Z0-9_]*)_test' $script_dir/../src/* | sed 's/^module //g')
 
 echo "####################################################################################"
 echo "#                           Running regression test                                #"
 echo "####################################################################################"
-echo "modules to test: $modules"
+echo -e "modules to test:\n$modules"
 
 # https://unix.stackexchange.com/questions/29509/transform-an-array-into-arguments-of-a-command
 vivado -mode batch -source $script_dir/run_unit_test.tcl -notrace -tclargs $modules
