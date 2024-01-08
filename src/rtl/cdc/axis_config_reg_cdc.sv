166debd5e90290f6756aad14c7dae1ac89db2472
Reed Foster <reed.a.foster@gmail.com>
2024-01-08 11:18:44 -0500

    add cdc for axi-stream configuration registers

---
+++ src/rtl/cdc/axis_config_reg_cdc.sv
@@ -0,0 +1,92 @@
+// axis_config_reg_cdc.sv - Reed Foster
+// Clock domain crossing for module configuration registers that use
+// AXI-stream
+
+module axis_config_reg_cdc #(
+  parameter int DWIDTH = 16
+) (
+  input wire src_clk, src_reset,
+  Axis_If.Master_Stream src,
+
+  input wire dest_clk, dest_reset,
+  Axis_If.Master_Stream dest
+);
+
+///////////////////////////////////////////////////
+// Source clock domain backpressure handling
+///////////////////////////////////////////////////
+logic src_send, src_rcv;
+logic [DWIDTH-1:0] src_data;
+// only allow new data if we aren't currently transferring data
+assign src.ready = ~(src_send | src_rcv);
+
+always_ff @(posedge src_clk) begin
+  src_data <= src.data;
+  if (src_reset) begin
+    src_send <= 1'b0;
+  end else begin
+    if (src.ok & ~src_rcv) begin
+      src_send <= 1'b1;
+    end else if (src_send & src_rcv) begin
+      src_send <= 1'b0;
+    end
+  end
+end
+
+///////////////////////////////////////////////////
+// Destination clock domain backpressure handling
+///////////////////////////////////////////////////
+logic dest_req, dest_ack;
+logic [DWIDTH-1:0] dest_data;
+logic transfer_done;
+
+always_ff @(posedge dest_clk) begin
+  dest.data <= dest_data;
+  if (dest_reset) begin
+    dest_ack <= 1'b0;
+    transfer_done <= 1'b0;
+    dest.valid <= 1'b0;
+  end else begin
+    // once dest_out has data, dest_req is asserted
+    // wait until ready goes high before sending ack
+    if (dest_req & dest.ready) begin
+      dest_ack <= 1'b1;
+      if (transfer_done) begin
+        // transfer occurred already, so deassert valid
+        dest.valid <= 1'b0;
+      end else begin
+        dest.valid <= 1'b1;
+        transfer_done <= 1'b1;
+      end
+    end else if (dest_ack & (~dest_req)) begin
+      // transaction is done when ack is still high
+      // but sync module deasserts req
+      // reset ack, also reset transfer_done
+      dest_ack <= 1'b0;
+      transfer_done <= 1'b0;
+    end
+  end
+end
+
+///////////////////////////////////////////////////
+// Synchronizer
+///////////////////////////////////////////////////
+xpm_cdc_handshake #(
+  .DEST_EXT_HSK(1), // use external handshake
+  .DEST_SYNC_FF(4), // four FF synchronizer for src->dest
+  .INIT_SYNC_FF(0), // disable behavioral initialization of sync FFs
+  .SIM_ASSERT_CHK(1), // enable simulation message reporting for misuse
+  .SRC_SYNC_FF(4), // four FF synchronizer for dest->src
+  .WIDTH(DWIDTH)
+) cdc_handshake_i (
+  .dest_clk,
+  .dest_out(dest_data),
+  .dest_req, // out
+  .dest_ack, // in
+  .src_clk,
+  .src_in(src_data),
+  .src_rcv, // out
+  .src_send // in
+);
+
+endmodule
---
+++ src/rtl/cdc/axis_config_reg_cdc_test.sv
@@ -0,0 +1,144 @@
+// axis_config_reg_cdc_test.sv - Reed Foster
+
+`timescale 1ns/1ps
+
+import sim_util_pkg::*;
+
+module axis_config_reg_cdc_test ();
+
+sim_util_pkg::debug debug = new(DEFAULT);
+
+Axis_If #(.DWIDTH(8)) f2s_src ();
+Axis_If #(.DWIDTH(8)) f2s_dest ();
+Axis_If #(.DWIDTH(8)) s2f_src ();
+Axis_If #(.DWIDTH(8)) s2f_dest ();
+
+logic fast_reset;
+logic fast_clk = 0;
+localparam FAST_CLK_RATE_HZ = 384_000_000;
+always #(0.5s/FAST_CLK_RATE_HZ) fast_clk = ~fast_clk;
+
+logic slow_reset;
+logic slow_clk = 0;
+localparam SLOW_CLK_RATE_HZ = 384_000_000;
+always #(0.5s/SLOW_CLK_RATE_HZ) slow_clk = ~slow_clk;
+
+axis_config_reg_cdc #(
+  .DWIDTH(8)
+) dut_f2s_i (
+  .src_clk(fast_clk),
+  .src_reset(fast_reset),
+  .dest_clk(slow_clk),
+  .dest_reset(slow_reset),
+  .src(f2s_src),
+  .dest(f2s_dest)
+);
+
+axis_config_reg_cdc #(
+  .DWIDTH(8)
+) dut_s2f_i (
+  .src_clk(slow_clk),
+  .src_reset(slow_reset),
+  .dest_clk(fast_clk),
+  .dest_reset(fast_reset),
+  .src(s2f_src),
+  .dest(s2f_dest)
+);
+
+logic [7:0] f2s_sent [$];
+logic [7:0] f2s_received [$];
+logic [7:0] s2f_sent [$];
+logic [7:0] s2f_received [$];
+
+logic f2s_enable_send, s2f_enable_send;
+
+always @(posedge fast_clk) begin
+  if (fast_reset) begin
+    f2s_src.data <= '0;
+  end else begin
+    f2s_src.valid <= $urandom() & f2s_enable_send;
+    s2f_dest.ready <= $urandom() & 1'b1;
+    if (f2s_src.ok) begin
+      f2s_sent.push_front(f2s_src.data);
+      f2s_src.data <= $urandom_range(0, 8'hff);
+    end
+    if (s2f_dest.ok) begin
+      s2f_received.push_front(s2f_dest.data);
+    end
+  end
+end
+
+always @(posedge slow_clk) begin
+  if (slow_reset) begin
+    s2f_src.data <= '0;
+  end else begin
+    s2f_src.valid <= $urandom() & s2f_enable_send;
+    f2s_dest.ready <= $urandom() & 1'b1;
+    if (s2f_src.ok) begin
+      s2f_sent.push_front(s2f_src.data);
+      s2f_src.data <= $urandom_range(0, 8'hff);
+    end
+    if (f2s_dest.ok) begin
+      f2s_received.push_front(f2s_dest.data);
+    end
+  end
+end
+
+task automatic check_results(
+  inout logic [7:0] sent [$],
+  inout logic [7:0] received [$]
+);
+  if (sent.size() !== received.size()) begin
+    debug.error($sformatf(
+      "sent.size() = %0d != received.size() = %0d",
+      sent.size(),
+      received.size())
+    );
+  end
+  while ((sent.size() > 0) & (received.size > 0)) begin
+    if (sent[$] !== received[$]) begin
+      debug.error($sformatf(
+        "sample mismatch, got %x expected %x",
+        received[$],
+        sent[$])
+      );
+    end
+    sent.pop_back();
+    received.pop_back();
+  end
+endtask
+
+initial begin
+  debug.display("### TESTING AXI-STREAM CONFIGURATION REGISTER CDC ###", DEFAULT);
+  slow_reset <= 1'b1;
+  fast_reset <= 1'b1;
+  s2f_enable_send <= 1'b0;
+  f2s_enable_send <= 1'b0;
+  repeat (100) @(posedge slow_clk);
+  slow_reset <= 1'b0;
+  @(posedge fast_clk);
+  fast_reset <= 1'b0;
+  
+  repeat (10) @(posedge slow_clk);
+  s2f_enable_send <= 1'b1;
+  @(posedge fast_clk);
+  f2s_enable_send <= 1'b1;
+
+  // send some data
+  repeat (10000) @(posedge slow_clk);
+  // stop sending data, finish any transfers in progress
+  s2f_enable_send <= 1'b0;
+  @(posedge fast_clk);
+  f2s_enable_send <= 1'b0;
+
+  repeat (50) @(posedge slow_clk);
+
+  debug.display("checking results for fast->slow CDC", VERBOSE);
+  check_results(f2s_sent, f2s_received);
+  debug.display("checking results for slow->fast CDC", VERBOSE);
+  check_results(s2f_sent, s2f_received);
+
+  debug.finish();
+end
+
+endmodule
