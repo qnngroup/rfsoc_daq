59760d9f1f32ec324e3acb734ff5e813a100a9e9
Reed Foster <reed.a.foster@gmail.com>
2024-01-25 14:08:45 -0500

    finish dual-clock buffer and add test

--- src/rtl/receive_chain/buffers/buffer_dual_clock.sv
+++ src/rtl/receive_chain/buffers/buffer_dual_clock.sv
@@ -20,34 +20,48 @@ module buffer_dual_clock #(
   output logic capture_full, // asserted when any active buffers* fill
   // *for non-independent banking modes (banking_mode < MAX), buffers are
   // chained together, increasing the write depth
-  output logic [CHANNELS-1:0][$clog2(BUFFER_DEPTH)-1:0] capture_write_addr,
+  output logic [CHANNELS-1:0][$clog2(BUFFER_DEPTH+1)-1:0] capture_write_depth,
   input wire capture_sw_reset, // manual software/PS-triggered reset
 
   // Readout (PS) clock, reset (100 MHz)
   input wire readout_clk, readout_reset,
-  Axis_Parallel_If.Master readout_data,
+  Axis_If.Master readout_data,
   // 
-  input wire readout_enable
+  input wire readout_sw_reset,
+  input wire readout_start
 
 );
 
 localparam int ADDR_WIDTH = $clog2(BUFFER_DEPTH);
-logic [DATA_WIDTH-1:0] memory [BUFFER_DEPTH][CHANNELS];
+logic [DATA_WIDTH-1:0] memory [CHANNELS][BUFFER_DEPTH];
 
 ////////////////////////////////////////////////////////
 // Capture clock domain
 ////////////////////////////////////////////////////////
 
 // register start/stop so that we can detect rising edge
-logic capture_start_reg, capture_stop_reg;
+logic capture_start_d, capture_stop_d;
 always_ff @(posedge capture_clk) begin
-  capture_start_reg <= capture_start;
-  capture_stop_reg <= capture_stop;
+  capture_start_d <= capture_start;
+  capture_stop_d <= capture_stop;
 end
 
-// manage writing to banks
-logic [CHANNELS-1:0][ADDR_WIDTH-1:0] capture_write_addr_d; // delay write address to match latency of valid/data pipeline registers
-logic [CHANNELS-1:0] capture_bank_full, capture_bank_full_latch; // track when banks are full
+// delay write address to match latency of valid/data pipeline registers
+logic [CHANNELS-1:0][ADDR_WIDTH-1:0] capture_write_addr, capture_write_addr_d;
+always_ff @(posedge capture_clk) capture_write_addr_d <= capture_write_addr;
+
+// track when banks are full
+logic [CHANNELS-1:0] capture_bank_full, capture_bank_full_latch;
+
+always_ff @(posedge capture_clk) begin
+  for (int channel = 0; channel < CHANNELS; channel++) begin
+    capture_write_depth[channel] <= {capture_bank_full_latch[channel], capture_write_addr[channel]};
+  end
+end
+
+// reset write address and full status when readout is done (CDC'd from
+// readout clock domain)
+logic capture_dma_done;
 
 // banking mode
 logic [$clog2($clog2(CHANNELS)+1)-1:0] capture_banking_mode_reg; // latch input banking mode when capture is started
@@ -58,31 +72,32 @@ assign capture_active_channels = 1'b1 << capture_banking_mode_reg; // number of 
 logic [CHANNELS-1:0] capture_full_mask, capture_valid_mask;
 
 // data in and muxed data to banks
-logic [CHANNELS-1:0][DATA_WIDTH-1:0] capture_data_reg, capture_buffer_in;
+logic [CHANNELS-1:0][DATA_WIDTH-1:0] capture_data_d, capture_buffer_in;
+
+always_ff @(posedge capture_clk) capture_data_d <= capture_data.data;
 
 // mux data based on banking_mode
 always_ff @(posedge capture_clk) begin
-  capture_data_reg <= capture_data.data;
   for (int channel = 0; channel < CHANNELS; channel++) begin
-    capture_buffer_in[channel] <= capture_data_reg[$clog2(CHANNELS)'(channel % capture_active_channels)];
+    capture_buffer_in[channel] <= capture_data_d[$clog2(CHANNELS)'($clog2(CHANNELS+1)'(channel) % capture_active_channels)];
   end
 end
 
 // valid->write_enable
-logic [CHANNELS-1:0] capture_valid_reg;
-logic [CHANNELS-1:0] capture_write_enable, capture_write_enable_reg;
+logic [CHANNELS-1:0] capture_valid_d;
+logic [CHANNELS-1:0] capture_write_enable, capture_write_enable_d;
 
 // valid->write_enable path (combinatorial, since we need to update address in a combinatorial loop ??)
 always_comb begin
   for (int channel = 0; channel < CHANNELS; channel++) begin
-    capture_write_enable[channel] = capture_start_reg & capture_valid_mask[channel]
-                                      & capture_valid_reg[$clog2(CHANNELS)'(channel % capture_active_channels)];
+    capture_write_enable[channel] = capture_start_d & capture_valid_mask[channel]
+                                      & capture_valid_d[$clog2(CHANNELS)'($clog2(CHANNELS+1)'(channel) % capture_active_channels)];
   end
 end
 // register write_enable and valid
 always_ff @(posedge capture_clk) begin
-  capture_valid_reg <= capture_data.valid;
-  capture_write_enable_reg <= capture_write_enable;
+  capture_valid_d <= capture_data.valid;
+  capture_write_enable_d <= capture_write_enable;
 end
 
 // latch banking mode on capture start
@@ -90,7 +105,7 @@ always_ff @(posedge capture_clk) begin
   if (capture_reset) begin
     capture_banking_mode_reg <= '0;
   end else begin
-    if (capture_start & ~capture_start_reg) begin
+    if (capture_start & ~capture_start_d) begin
       capture_banking_mode_reg <= capture_banking_mode;
     end
   end
@@ -100,14 +115,14 @@ always_ff @(posedge capture_clk) begin
   if (capture_reset) begin
     capture_full_mask <= '0;
   end else begin
-    if (capture_start & ~capture_start_reg) begin
+    if (capture_start & ~capture_start_d) begin
       for (int channel = 0; channel < CHANNELS; channel++) begin
         // For 8 channels:
         // if banking_mode == 0 (1 active channels): mask = 0x80 (10000000)
         // if banking_mode == 1 (2 active channels): mask = 0xc0 (11000000)
         // if banking_mode == 2 (4 active channels): mask = 0xf0 (11110000)
         // if banking_mode == 3 (8 active channels): mask = 0xff (11111111)
-        capture_full_mask[CHANNELS-1-channel] <= channel + 1 > (1 << capture_banking_mode) ? 1'b1 : 1'b0;
+        capture_full_mask[CHANNELS-1-channel] <= channel + 1 > (1 << capture_banking_mode) ? 1'b0 : 1'b1;
       end
     end
   end
@@ -116,19 +131,18 @@ end
 // update write address
 always_comb begin
   for (int channel = 0; channel < CHANNELS; channel++) begin
-    capture_bank_full[channel] = capture_write_addr == $clog2(BUFFER_DEPTH)'(BUFFER_DEPTH - 1);
+    capture_bank_full[channel] = capture_write_addr[channel] == $clog2(BUFFER_DEPTH)'(BUFFER_DEPTH - 1) & capture_write_enable[channel];
   end
 end
 always_ff @(posedge capture_clk) begin
-  capture_write_addr_d <= capture_write_addr;
   if (capture_reset) begin
     capture_write_addr <= '0;
   end else begin
-    if (capture_sw_reset | capture_readout_done) begin
+    if (capture_sw_reset | capture_dma_done) begin
       capture_write_addr <= '0;
     end else begin
       for (int channel = 0; channel < CHANNELS; channel++) begin
-        if (capture_write_enable[channel] & ~capture_bank_full[channel]) begin
+        if (capture_write_enable[channel]) begin// & ~capture_bank_full[channel]) begin
           capture_write_addr[channel] <= capture_write_addr[channel] + 1;
         end
       end
@@ -142,14 +156,14 @@ always_ff @(posedge capture_clk) begin
     capture_valid_mask <= '0;
   end else begin
     for (int channel = 0; channel < CHANNELS; channel++) begin
-      if (capture_start & ~capture_start_reg) begin
-        // initialize to same as capture_full_mask
+      if (capture_start & ~capture_start_d) begin
+        // initialized almost same as capture_full_mask, just bitswapped
         // For 8 channels:
-        // if banking_mode == 0 (1 active channels): mask = 0x80 (10000000)
-        // if banking_mode == 1 (2 active channels): mask = 0xc0 (11000000)
-        // if banking_mode == 2 (4 active channels): mask = 0xf0 (11110000)
+        // if banking_mode == 0 (1 active channels): mask = 0x01 (10000000)
+        // if banking_mode == 1 (2 active channels): mask = 0x03 (11000000)
+        // if banking_mode == 2 (4 active channels): mask = 0x0f (11110000)
         // if banking_mode == 3 (8 active channels): mask = 0xff (11111111)
-        capture_valid_mask[CHANNELS-1-channel] <= channel + 1 > (1 << capture_banking_mode) ? 1'b1 : 1'b0;
+        capture_valid_mask[channel] <= (channel + 1 > (1 << capture_banking_mode)) ? 1'b0 : 1'b1;
       end else begin
         // when a bank fills up, update which bank is active by shifting the
         // bits in the mask based on the current banking mode
@@ -160,8 +174,8 @@ always_ff @(posedge capture_clk) begin
             // if we're resetting the current valid mask (i.e. it wasn't reset
             // in a previous clock cycle), then set a subsequent mask bit whose
             // distance from the current bit is determined by the banking mode
-            if (channel + capture_active_channels < CHANNELS) begin
-              capture_valid_mask[channel + capture_active_channels] <= 1'b1;
+            if ($clog2(CHANNELS+1)'(channel) + capture_active_channels < $clog2(CHANNELS+1)'(CHANNELS)) begin
+              capture_valid_mask[$clog2(CHANNELS)'($clog2(CHANNELS+1)'(channel) + capture_active_channels)] <= 1'b1;
             end
           end
         end
@@ -175,7 +189,7 @@ always_ff @(posedge capture_clk) begin
   if (capture_reset) begin
     capture_bank_full_latch <= '0;
   end else begin
-    if (capture_sw_reset | capture_readout_done) begin
+    if (capture_sw_reset | capture_dma_done) begin
       capture_bank_full_latch <= '0;
     end else begin
       for (int channel = 0; channel < CHANNELS; channel++) begin
@@ -187,12 +201,129 @@ always_ff @(posedge capture_clk) begin
   end
 end
 
-assign capture_full = |capture_bank_full_latch;
+assign capture_full = |(capture_full_mask & capture_bank_full_latch);
 
 ////////////////////////////////////////////////////////
 // Readout clock domain
 ////////////////////////////////////////////////////////
-logic [CHANNELS-1:0][ADDR_WIDTH-1:0] readout_read_addr;
+logic [ADDR_WIDTH-1:0] readout_read_addr;
+logic [$clog2(CHANNELS)-1:0] readout_bank_select;
+
+logic [CHANNELS-1:0][READ_LATENCY-1:0][DATA_WIDTH-1:0] readout_data_pipe;
+logic [READ_LATENCY-1:0][$clog2(CHANNELS)-1:0] readout_bank_select_pipe;
+logic [READ_LATENCY-1:0] readout_valid_pipe, readout_last_pipe;
+
+logic readout_enable, readout_active;
+logic readout_last;
+
+assign readout_last = (readout_read_addr == ADDR_WIDTH'(BUFFER_DEPTH - 1)) & (readout_bank_select == $clog2(CHANNELS)'(CHANNELS - 1));
+assign readout_enable = readout_active & (readout_data.ready | ~readout_data.valid);
+
+logic readout_start_d;
+always_ff @(posedge readout_clk) readout_start_d <= readout_start;
+logic readout_dma_done;
+assign readout_dma_done = readout_data.ok & readout_data.last;
+
+// pipeline for valid/last and bank_select
+always_ff @(posedge readout_clk) begin
+  if (readout_reset) begin
+    readout_valid_pipe <= '0;
+    readout_last_pipe <= '0;
+    readout_data.valid <= '0;
+    readout_data.last <= '0;
+    readout_bank_select_pipe <= '0;
+  end else begin
+    if (readout_data.ready | ~readout_data.valid) begin
+      readout_valid_pipe <= {readout_valid_pipe[READ_LATENCY-2:0], readout_active};
+      readout_last_pipe <= {readout_last_pipe[READ_LATENCY-2:0], readout_last};
+      readout_data.valid <= readout_valid_pipe[READ_LATENCY-1];
+      readout_data.last <= readout_last_pipe[READ_LATENCY-1];
+      readout_bank_select_pipe <= {readout_bank_select_pipe[READ_LATENCY-2:0], readout_bank_select};
+    end
+  end
+end
+
+// update read address and bank select
+always_ff @(posedge readout_clk) begin
+  if (readout_reset) begin
+    readout_read_addr <= '0;
+    readout_bank_select <= '0;
+  end else begin
+    if (readout_sw_reset | (readout_start & ~readout_start_d)) begin
+      readout_read_addr <= '0;
+      readout_bank_select <= '0;
+    end else begin
+      if (readout_enable) begin
+        if (readout_read_addr == ADDR_WIDTH'(BUFFER_DEPTH - 1)) begin
+          readout_read_addr <= 0;
+          if (readout_bank_select == $clog2(CHANNELS)'(CHANNELS - 1)) begin
+            readout_bank_select <= 0;
+          end else begin
+            readout_bank_select <= readout_bank_select + 1;
+          end
+        end else begin
+          readout_read_addr <= readout_read_addr + 1;
+        end
+      end
+    end
+  end
+end
+
+// SR flip-flop for readout_active
+always_ff @(posedge readout_clk) begin
+  if (readout_reset) begin
+    readout_active <= 1'b0;
+  end else begin
+    if (readout_start & ~readout_start_d) begin
+      readout_active <= 1'b1;
+    end else if (readout_last & readout_data.ready) begin
+      readout_active <= 1'b0;
+    end
+  end
+end
+
+// mux output
+always_ff @(posedge readout_clk) begin
+  if (readout_data.ready | ~readout_data.valid) begin
+    readout_data.data <= readout_data_pipe[readout_bank_select_pipe[READ_LATENCY-1]][READ_LATENCY-1];
+  end
+end
+
+////////////////////////////////////////////////////////
+// Clock crossing and memory
+////////////////////////////////////////////////////////
+
+xpm_cdc_pulse #(
+  .DEST_SYNC_FF(4), // 4 synchronization stages
+  .INIT_SYNC_FF(0), // don't allow behavioral initialization
+  .REG_OUTPUT(1), // register the output
+  .RST_USED(1), // use resets
+  .SIM_ASSERT_CHK(1) // report potential violations
+) dma_done_cdc_i (
+  .src_clk(readout_clk),
+  .src_rst(readout_reset),
+  .src_pulse(readout_dma_done),
+  .dest_clk(capture_clk),
+  .dest_rst(capture_reset),
+  .dest_pulse(capture_dma_done)
+);
+
+always_ff @(posedge capture_clk) begin
+  for (int channel = 0; channel < CHANNELS; channel++) begin
+    // write to memory
+    if (capture_write_enable_d[channel]) begin
+      memory[channel][capture_write_addr_d[channel]] <= capture_buffer_in[channel];
+    end
+  end
+end
+
+always_ff @(posedge readout_clk) begin
+  for (int channel = 0; channel < CHANNELS; channel++) begin
+    if (readout_data.ready | ~readout_data.valid) begin
+      readout_data_pipe[channel] <= {readout_data_pipe[channel][READ_LATENCY-2:0], memory[channel][readout_read_addr]};
+    end
+  end
+end
 
 
 endmodule
---
+++ src/rtl/receive_chain/buffers/buffer_dual_clock_test.sv
@@ -0,0 +1,255 @@
+// buffer_dual_clock_test.sv - Reed Foster
+
+`timescale 1ns/1ps
+module buffer_dual_clock_test ();
+
+sim_util_pkg::debug debug = new(sim_util_pkg::DEBUG);
+
+localparam int CHANNELS = 4;
+localparam int BUFFER_DEPTH = 64;
+localparam int DATA_WIDTH = 16;
+localparam int READ_LATENCY = 4;
+
+logic capture_reset;
+logic capture_clk = 0;
+localparam CAPTURE_CLK_RATE_HZ = 512_000_000;
+always #(0.5s/CAPTURE_CLK_RATE_HZ) capture_clk = ~capture_clk;
+
+logic readout_reset;
+logic readout_clk = 0;
+localparam READOUT_CLK_RATE_HZ = 100_000_000;
+always #(0.5s/READOUT_CLK_RATE_HZ) readout_clk = ~readout_clk;
+
+Realtime_Parallel_If #(.DWIDTH(DATA_WIDTH), .CHANNELS(CHANNELS)) capture_data ();
+Axis_If #(.DWIDTH(DATA_WIDTH)) readout_data ();
+
+logic [$clog2($clog2(CHANNELS)+1)-1:0] capture_banking_mode;
+logic capture_start, capture_stop;
+logic capture_full;
+
+logic [CHANNELS-1:0][$clog2(BUFFER_DEPTH+1)-1:0] capture_write_depth;
+logic capture_sw_reset;
+
+logic readout_sw_reset;
+logic readout_start;
+
+buffer_dual_clock #(
+  .CHANNELS(CHANNELS),
+  .BUFFER_DEPTH(BUFFER_DEPTH),
+  .DATA_WIDTH(DATA_WIDTH),
+  .READ_LATENCY(READ_LATENCY)
+) dut_i (
+  .capture_clk,
+  .capture_reset,
+  .capture_data,
+  .capture_banking_mode,
+  .capture_start,
+  .capture_stop,
+  .capture_full,
+  .capture_write_depth,
+  .capture_sw_reset,
+  .readout_clk,
+  .readout_reset,
+  .readout_data,
+  .readout_sw_reset,
+  .readout_start
+);
+
+logic readout_enabled;
+
+logic [DATA_WIDTH-1:0] samples_sent [CHANNELS][$];
+logic [DATA_WIDTH-1:0] samples_received [$];
+int last_received;
+
+always @(posedge capture_clk) begin
+  capture_data.valid <= $urandom_range('0, {CHANNELS{1'b1}});
+  if (capture_start) begin
+    for (int channel = 0; channel < CHANNELS; channel++) begin
+      if (capture_data.valid[channel]) begin
+        capture_data.data[channel] <= $urandom_range(0, {DATA_WIDTH{1'b1}});
+        samples_sent[channel].push_front(capture_data.data[channel]);
+      end
+    end
+  end
+end
+
+always @(posedge readout_clk) begin
+  if (readout_enabled) begin
+    readout_data.ready <= $urandom_range(0, 1);
+  end
+end
+
+always @(posedge readout_clk) begin
+  if (readout_reset) begin
+    last_received <= 0;
+  end else begin
+    if (capture_start) begin
+      last_received <= 0;
+    end else begin
+      if (readout_data.ok) begin
+        samples_received.push_front(readout_data.data);
+        if (readout_data.last) begin
+          last_received <= samples_received.size();
+        end
+      end
+    end
+  end
+end
+
+task automatic check_write_depth (
+  input logic [DATA_WIDTH-1:0] samples_sent [CHANNELS][$],
+  input logic [CHANNELS-1:0][$clog2(BUFFER_DEPTH+1)-1:0] write_depths,
+  input int banking_mode
+);
+  
+  int total_write_depth;
+
+  // make sure the write depths match up with the number of samples sent
+  for (int channel = 0; channel < 1 << banking_mode; channel++) begin
+    total_write_depth = 0;
+    for (int bank = channel; bank < CHANNELS; bank += 1 << banking_mode) begin
+      if (write_depths[bank][$clog2(BUFFER_DEPTH+1)-1]) begin
+        // if MSB is set, then bank is full
+        total_write_depth += BUFFER_DEPTH;
+      end else begin
+        total_write_depth += write_depths[bank];
+      end
+    end
+    if (samples_sent[channel].size() !== total_write_depth) begin
+      debug.error($sformatf(
+        "channel %0d: sent %0d samples, but %0d were written",
+        channel,
+        samples_sent[channel].size(),
+        total_write_depth)
+      );
+    end
+  end
+
+endtask
+
+task automatic check_results (
+  inout logic [DATA_WIDTH-1:0] samples_sent [CHANNELS][$],
+  inout logic [DATA_WIDTH-1:0] samples_received [$],
+  input int last_received,
+  input int banking_mode
+);
+ 
+  // keep track of what sample we're on across multiple banks
+  int sample_index;
+
+  // make sure we got tlast at the right time
+  if (last_received !== CHANNELS*BUFFER_DEPTH) begin
+    debug.error($sformatf(
+      "expected tlast event on cycle %0d, got it on %0d",
+      CHANNELS*BUFFER_DEPTH,
+      last_received)
+    );
+  end
+
+  // make sure samples_received is the right size
+  if (samples_received.size() !== CHANNELS*BUFFER_DEPTH) begin
+    debug.error($sformatf(
+      "expected to receive %0d samples, but got %0d",
+      CHANNELS*BUFFER_DEPTH,
+      samples_received.size())
+    );
+  end
+
+  // check that the right data was received
+  for (int bank = 0; bank < CHANNELS; bank++) begin
+    for (int sample = 0; sample < BUFFER_DEPTH; sample++) begin
+      sample_index = (bank / (1 << banking_mode)) * BUFFER_DEPTH + sample;
+      if (sample_index < samples_sent[bank % (1 << banking_mode)].size()) begin
+        if (samples_sent[bank % (1 << banking_mode)][$-sample_index] !== samples_received[$]) begin
+          debug.error($sformatf(
+            "channel %0d, bank %0d: sample mismatch, expected %x got %x",
+            bank % (1 << banking_mode),
+            bank,
+            samples_sent[bank % (1 << banking_mode)][$-sample_index],
+            samples_received[$])
+          );
+        end
+      end
+      samples_received.pop_back();
+    end
+  end
+
+  // clean up extra samples
+  for (int channel = 0; channel < CHANNELS; channel++) begin
+    while (samples_sent[channel].size() > 0) samples_sent[channel].pop_back();
+  end
+  while (samples_received.size() > 0) samples_received.pop_back(); 
+
+endtask
+
+initial begin
+  debug.display("### TESTING DUAL-CLOCK SAMPLE BUFFER ###", sim_util_pkg::DEFAULT);
+
+  // assert resets
+  readout_reset <= 1'b1;
+  capture_reset <= 1'b1;
+
+  // reset inputs to DUT
+  readout_data.ready <= 1'b0;
+  capture_banking_mode <= '0;
+  capture_start <= 1'b0;
+  capture_stop <= 1'b0;
+  capture_sw_reset <= 1'b0;
+  readout_sw_reset <= 1'b0;
+  readout_start <= 1'b0;
+
+  repeat (100) @(posedge readout_clk);
+  readout_reset <= 1'b0;
+  repeat (10) @(posedge capture_clk);
+  capture_reset <= 1'b0;
+
+  for (int sample_count_mode = 0; sample_count_mode < 2; sample_count_mode++) begin
+    if (sample_count_mode == 0) begin
+      debug.display("sending a few samples", sim_util_pkg::VERBOSE);
+    end else begin
+      debug.display("sending samples until full", sim_util_pkg::VERBOSE);
+    end
+    for (int banking_mode = 0; banking_mode <= $clog2(CHANNELS); banking_mode++) begin
+      debug.display($sformatf("testing with banking_mode = %0d", banking_mode), sim_util_pkg::VERBOSE);
+      // set the banking mode
+      capture_banking_mode <= banking_mode;
+      @(posedge capture_clk);
+      // start capture
+      capture_start <= 1'b1;
+      @(posedge capture_clk)
+      // send samples
+      // wait some amount of time
+      debug.display("waiting for capture to complete", sim_util_pkg::DEBUG);
+      if (sample_count_mode == 0) begin
+        repeat ($urandom_range(5, 20)) @(posedge capture_clk);
+        capture_stop <= 1'b1;
+        capture_start <= 1'b0;
+      end else begin
+        while (capture_start) begin
+          for (int channel = 0; channel < 1 << banking_mode; channel++) begin
+            if (samples_sent[channel].size() == (BUFFER_DEPTH * (CHANNELS >> banking_mode))) begin
+              capture_start <= 1'b0;
+            end
+          end
+          @(posedge capture_clk);
+        end
+      end
+      @(posedge readout_clk);
+      readout_start <= 1'b1;
+      readout_enabled <= 1'b1;
+      debug.display("waiting for readout to complete", sim_util_pkg::DEBUG);
+      repeat (10) @(posedge readout_clk);
+      // check capture_write_depth
+      check_write_depth(samples_sent, capture_write_depth, banking_mode);
+      while (~(readout_data.ok & readout_data.last)) @(posedge readout_clk);
+      readout_start <= 1'b0;
+      readout_enabled <= 1'b0;
+      repeat (10) @(posedge readout_clk);
+      check_results(samples_sent, samples_received, last_received, banking_mode);
+    end
+  end
+
+  debug.finish();
+end
+
+endmodule
