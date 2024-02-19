27b4fa50444a86b42c1f1fb80385064a763432b4
Reed Foster <reed.a.foster@gmail.com>
2024-02-19 09:51:14 -0500

    improve regex for test/testbench code

--- script/get_compilation_order.sh
+++ script/get_compilation_order.sh
@@ -2,10 +2,12 @@
 
 script_dir=$(dirname "$(realpath $0)")
 list_file=$script_dir/../build/file_order.lst
+verilog_regex="\.\(v\|sv\)"
+test_regex=".*_\(test\|tb\)"$verilog_regex
 
 echo $script_dir/null_modules/xpm_cdc.sv > $list_file
-# first, all the synthesizable RTL
-find $script_dir/../src/rtl/ -type f ! -regex ".*_test\.sv" >> $list_file
+# first, all the synthesizable RTL (v, sv sources that are not tests)
+find $script_dir/../src/rtl/ -type f \( ! -regex $test_regex -a -regex ".*"$verilog_regex \) >> $list_file
 # next, sim_util_pkg
 echo $script_dir/../src/verif/sim_util_pkg.sv >> $list_file
 # then, sample_discriminator_pkg
@@ -13,7 +15,7 @@ echo $script_dir/../src/verif/sample_discriminator_pkg.sv >> $list_file
 # then all verification utilites/classes
 find $script_dir/../src/verif/ -type f -regex ".*\.sv" >> $list_file
 # finally the unit tests
-find $script_dir/../src/rtl/ -type f -regex ".*_test\.sv" >> $list_file
+find $script_dir/../src/rtl/ -type f -regex $test_regex >> $list_file
 
 # https://stackoverflow.com/questions/11532157/remove-duplicate-lines-without-sorting
 awk '!seen[$0]++' $list_file | xargs realpath > $list_file.sorted
