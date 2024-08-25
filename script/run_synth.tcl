ff93f19b1c9a4e60da082064251af83ea99a7b8d
Reed Foster <reed.a.foster@gmail.com>
2024-08-25 10:09:57 -0400

    change synth varsize limit

--- script/run_synth.tcl
+++ script/run_synth.tcl
@@ -4,7 +4,8 @@ set proj_dir [file normalize $dir/../build/proj_synth]
 
 open_project $proj_dir/proj_synth.xpr
 
-set_param synth.elaboration.rodinMoreOptions "rt::set_parameter var_size_limit 4194304"
+# max 100 Mbit variable size
+set_param synth.elaboration.rodinMoreOptions "rt::set_parameter var_size_limit 100000000"
 set_param messaging.defaultLimit 5000
 
 reset_run synth_1
